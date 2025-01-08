af09ad93290837ee02fe174d07dd49b7d8c5eed5
jure-s <71380578+jure-s@users.noreply.github.com>
2025-01-09 02:30:59 +0400

    Add validation module for phone, email, and birthday

--- models/validation.py
+++ models/validation.py
@@ -2,17 +2,26 @@ import re
 from datetime import datetime
 
 def validate_phone(phone):
+    """
+    Перевіряє, чи номер телефону складається з 10 цифр.
+    """
     if not phone.isdigit() or len(phone) != 10:
         raise ValueError("Phone number must have exactly 10 digits.")
     return phone
 
 def validate_email(email):
+    """
+    Перевіряє формат email.
+    """
     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
     if not re.match(pattern, email):
         raise ValueError("Invalid email format.")
     return email
 
 def validate_birthday(birthday):
+    """
+    Перевіряє формат дати народження (DD.MM.YYYY).
+    """
     try:
         return datetime.strptime(birthday, "%d.%m.%Y").date()
     except ValueError:
